33e744d5deeca9ea6ef70d6c552b540d1bc4a5e6
x.ande <ananfro@live.com>
2023-08-14 18:11:34 +1000

    update

--- index.js
+++ index.js
@@ -4,44 +4,127 @@ const fetch = require("node-fetch");
 async function getRequest(endpoint) {
 
     const res = await fetch(`http://localhost:3001/api/${endpoint}/`, { method: "GET" });
-    console.log(await res.json());
+    return await res.json();
+
+}
+
+async function getRoleAsArray() {
+
+    const roles = await getRequest("role");
+
+    let rolesArray = [];
+
+    for (const item of await roles) {
+
+        rolesArray.push(item.title);
+
+    }
+
+    return rolesArray;
+
+}
+
+async function getEmployeesAsArray() {
+
+    const employees = await getRequest("employee");
+
+    let employeesArray = [];
+
+    for (const item of await employees) {
+
+        employeesArray.push(`${item.first_name} ${item.last_name}`);
+
+    }
+
+    return employeesArray;
 
 }
 
 async function goToMainMenu() {
 
-    const mainMenu = await inquirer.prompt(
+    const menuChoices = [
+        "View Employee Register", "View Job Roles", "View Departments",
+        "Add Employee", "Add Job Role", "Add Department"
+    ];
+
+    const mainMenu = await inquirer.prompt([
 
         {
             type: "list",
             message: "What would you like to do?",
-            choices: ["View Employee Register", "View Job Roles", "View Departments"],
+            choices: menuChoices,
             name: "menuOption",
         }
 
-    );
+    ]);
 
     const y = console.log(mainMenu.menuOption);
-
-    // async function () {
 
     switch (mainMenu.menuOption) {
 
         case "View Employee Register":
-            getRequest("employee");
+            console.log(await getRequest("employee"));
             break;
 
         case "View Job Roles":
-            getRequest("role");
+            console.log(await getRequest("role"));
             break;
 
         case "View Departments":
-            getRequest("department");
+            console.log(await getRequest("department"));
+            break;
+
+        case "Add Employee":
+            const addEmployee = await inquirer.prompt([
+
+                {
+                    type: "input",
+                    message: "First name of the employee...",
+                    name: "firstName",
+                },
+
+                {
+                    type: "input",
+                    message: "Last name of the employee...",
+                    name: "lastName",
+                },
+
+                {
+                    type: "list",
+                    message: "Role of the employee...",
+                    choices: await getRoleAsArray(),
+                    name: "employeeRole",
+                },
+
+                {
+                    type: "list",
+                    message: "Manager of the employee...",
+                    choices: await getEmployeesAsArray(),
+                    name: "employeeManager",
+                }
+
+            ]);
+
+            const body = await addEmployee;
+
+            fetch("http://localhost:3001/api/employee",
+                {
+                    method: "POST",
+                    body: JSON.stringify(body),
+                    headers: { "Content-Type": "application/json" }
+                });
+
+            break;
+
+        case "Add Job Role":
+
+            break;
+
+        case "Add Department":
+
             break;
 
     }
-
-    goToMainMenu();
 
 }
 
--- routes/api/employee/index.js
+++ routes/api/employee/index.js
@@ -1,9 +1,9 @@
 const employee = require("express").Router();
 const mysql = require("mysql2/promise");
 
-async function x(res) {
+async function getRoleAsId(role) {
 
-    const pool = mysql.createPool({
+    const pool = await mysql.createConnection({
 
         connectionLimit: 10,
         host: "localhost",
@@ -17,13 +17,75 @@ async function x(res) {
 
     );
 
-    let x = await pool.promise().query(`
+    let yyy = await pool.query(`
+
+    SELECT * FROM role;
+
+    `)
+
+    for (let i = 0; i < yyy[0].length; i++) {
+
+        if (role === yyy[0][i].title) return yyy[0][i].id;
+
+    }
+
+}
+
+async function getManagerId(manager) {
+
+    const pool = await mysql.createConnection({
+
+        connectionLimit: 10,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "employee_register_db",
+
+    },
+
+        console.log(`Pool opened`)
+
+    );
+
+    let yyy = await pool.query(`
 
     SELECT * FROM employee;
 
-    `, (err, results) => results);
+    `)
 
-    res.send(x);
+    let lol = manager.split(" ")
+
+    for (let i = 0; i < yyy[0].length; i++) {
+
+        if (lol[0] === yyy[0][i].first_name && lol[1] === yyy[0][i].last_name) return yyy[0][i].id;
+
+    }
+
+}
+
+async function x(res) {
+
+    const pool = await mysql.createPool({
+
+        connectionLimit: 10,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "employee_register_db",
+
+    },
+
+        console.log(`Pool opened`)
+
+    );
+
+    const query = await pool.query(`
+
+    SELECT * FROM employee;
+
+    `)
+
+    res.send(query[0]);
 
 }
 
@@ -32,5 +94,44 @@ employee.get("/", (req, res) => {
     x(res);
 
 });
+
+// {
+//     firstName: 'x',
+//     lastName: 'x',
+//     employeeRole: 'Receptionist',
+//     employeeManager: 'Alexander Nanfro'
+//   }
+
+employee.post("/", async (req, res) => {
+
+    const pool = await mysql.createPool({
+
+        connectionLimit: 10,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "employee_register_db",
+
+    },
+
+        console.log(`Pool opened`)
+
+    );
+
+    const { firstName, lastName, employeeRole, employeeManager } = req.body;
+    let roleId = await getRoleAsId(employeeRole);
+    let managerId = await getManagerId(employeeManager);
+
+    const query = await pool.query(`
+
+    INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES  ("${firstName}", "${lastName}", ${roleId}, ${managerId});
+
+    `);
+    yyy[0][i].last_name
+    res.send("Employee added successfully!");
+
+});
+
 
 module.exports = employee;
