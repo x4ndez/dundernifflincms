c65217ce514bee0e998789a1917e7fbde00e1d47
x.ande <ananfro@live.com>
2023-08-19 11:43:54 +1000

    added query for a few bonus questions, not applied in application

--- db/query.sql
+++ db/query.sql
@@ -1,11 +1,3 @@
-SELECT e.id, e.first_name, e.last_name, r.title AS role, e.manager_id, d.name
-FROM ((employee e
-JOIN role r
-ON e.role_id = r.id)
-JOIN department d
-ON r.department_id = d.id)
-ORDER BY e.manager_id;
-
 -- ADD DEPARTMENT
 INSERT INTO department (name)
 VALUES ("");
@@ -23,3 +15,29 @@ WHERE id = "";
 SELECT *
 FROM employee
 WHERE manager_id = x;
+
+-- GET LIST OF EMPLOYEES BY DEPARTMENT
+SELECT e.id, e.first_name, e.last_name, r.title, r.department_id
+FROM employee e
+JOIN role r
+ON e.role_id = r.id
+WHERE department_id = 1;
+
+-- View the total utilized budget of a department—in other words,
+-- the combined salaries of all employees in that department.
+SELECT SUM(salary)
+FROM role
+WHERE department_id = x;
+
+-- Delete departments, roles, and employees.
+DELETE FROM employee
+WHERE id = x;
+
+DELETE FROM department
+WHERE id = x;
+
+-- must change the employee's role that share the id to be deleted before deleting
+-- OR CASCADE, then read the employee
+-- but I think for this application, changing an employee role first would be safer.
+DELETE FROM role
+WHERE id = x;
